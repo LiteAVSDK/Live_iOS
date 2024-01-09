449d0a186ea5e85f7cee0fb882e5685c00af3a73
adamsfliu <adamsfliu@tencent.com>
2024-01-09 17:11:27 +0800

    [iOS] update version to relesse-11.6.0.4703

---
+++ MLVB-API-Example-OC/Advanced/PictureInPicture/PictureInPictureView.h
@@ -0,0 +1,17 @@
+//
+//  PictureInPictureView.h
+//  MLVB-API-Example-OC
+//
+//  Created by adams on 2023/9/18.
+//  Copyright © 2023 Tencent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PictureInPictureView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
